b86d304ab47ed6707eea11f1d36c0cc92fbdf3c8
sandrabosk <sandraboskovic@hotmail.com>
2022-02-10 13:08:40 +0100

    remove web stuff

--- IronHack-Web-Module-2-Project/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-  defaultGenderImage: 'male-female.png',
-  defaultAvatar: 'default-avatar.png'
-};
-
-module.exports = {
-  constants
-};
--- IronHack-Web-Tunathon-2018/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function main () {
-  console.log('JS Running');
-}
-
-window.addEventListener('load', main);
